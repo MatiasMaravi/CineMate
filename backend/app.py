a0c3f23e3faafad3c508f3e1d0df20bfc7682de9
MatiasMaravi <matias.maravi@utec.edu.pe>
2023-10-27 14:38:27 -0500

    terminado

--- backend/app.py
+++ backend/app.py
@@ -17,12 +17,18 @@ app = Flask(__name__)
 @app.route('/movies', methods=['POST'])
 def get_movies():
     try:
-        n = int(n)
-        with open('movies.json', 'r') as json_file:
-            data = json.load(json_file)
-        return jsonify(data["movies"][:n])
+        request_data = request.json
+        movie_title = request_data['name']
+        recommendations = get_genre_recommendations(movie_title, n=5)
+        # Obtener los nombres de las películas recomendadas
+        recommended_movie_titles = recommendations.index
+        dictionary = {"movies":[]}
+        for i in recommended_movie_titles:
+            dictionary["movies"].append(i)
+        return jsonify(dictionary)
     except FileNotFoundError:
         return jsonify({"error": "El archivo 'movies.json' no se encontró."}), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
