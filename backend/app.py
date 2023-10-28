add20bf0c8efc23eaacad229fac087808a1ed384
MatiasMaravi <matias.maravi@utec.edu.pe>
2023-10-27 19:09:48 -0500

    Requerimiento 1 finalizado

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,15 @@
 from recommendation.core import get_genre_recommendations
 from flask import Flask, jsonify, request
-import supabase
+from supabase import create_client, Client
 import os 
 from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
 
-url = "https://inlhowinxzuskmodrpix.supabase.co"
-key = os.getenv("KEY") #"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlubGhvd2lueHp1c2ttb2RycGl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY5NjI5ODYsImV4cCI6MjAxMjUzODk4Nn0.jII7SbPyktiAuUrKirqQ6eD7_2uu-Mb8crpnT0MkMfo"
-client = supabase.Client(url, key)
+url = os.getenv("URL")
+key = os.getenv("KEY")
+supabase: Client = create_client(url, key)
 
 
 
@@ -17,19 +17,17 @@ client = supabase.Client(url, key)
 def get_movies():
     try:
         request_data = request.json
-        movie_title = request_data['name']
+        movie_title = request_data['title']
         recommendations = get_genre_recommendations(movie_title, n=5)
         # Obtener los nombres de las películas recomendadas
         recommended_movie_titles = recommendations.index
+        
 
         for title in recommended_movie_titles:
-            data_to_insert = [{"name": title}]
-            response, error = client.table("movies").upsert(data_to_insert, returning="representation")
-            if error:
-                return jsonify({"error" : f"No se pudo insertar la película: {title}"})
-            
-        dictionary = {"movies": recommended_movie_titles}  
-        return jsonify(dictionary)
+            data_to_insert = {"title": title}
+            data, count = supabase.table("movies").insert(data_to_insert).execute()
+
+        return jsonify({"response": "ok"}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 404
     
