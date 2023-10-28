de9ee4a1b1d7fdd0f4510e0607f32e24dee53431
Leonardo Isidro <leoisidro@MacBook-Air-de-Leonardo.local>
2023-10-28 18:50:10 -0500

    Avance IA

---
+++ backend/IA/api_chat.py
@@ -0,0 +1,56 @@
+import openai
+import time
+
+clave = "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"
+
+# Configura la API key
+openai.api_key = clave
+
+def IA(generos):
+
+    # Genera una respuesta a partir de un prompt
+
+    promt="Recomiendame solo los nombres de 5  peliculas sin informacion antes ni despues , de generos de "
+    
+    for i in generos:
+        promt = promt + i + ","
+    
+    promt+="y devuelvemelo en formato python de lista, en una sola linea, sin informacion extra, sin corchetes, separado por comas, sin comillas."
+
+    print(promt)
+
+    start_time = time.time()
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": promt },
+        ],
+        temperature=0.7
+    )
+
+    end_time = time.time()
+
+    print("Tiempo de ejecucion: ", end_time - start_time , " segundos")
+
+    # Extraer la respuesta generada por el modelo, la cual se encuentra en el primer elemento de la lista
+    respuesta_generada = response['choices'][0]['message']['content']
+
+    # Mostrar la respuesta
+    respuesta_generada = respuesta_generada.split(",")
+
+    respuesta={
+        "movies":respuesta_generada,
+        "time": end_time - start_time
+    }
+
+    return respuesta
+
+
+respuesta= IA(["Terror","Comedia"])
+
+print(respuesta)
+
+
+
+
--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,20 @@
 from recommendation.core import get_genre_recommendations
 from flask import Flask, jsonify, request
 from supabase import create_client, Client
+from IA.api_chat import IA
 import os 
 from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
 
-url = os.getenv("URL")
-key = os.getenv("KEY")
+url = "https://inlhowinxzuskmodrpix.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlubGhvd2lueHp1c2ttb2RycGl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY5NjI5ODYsImV4cCI6MjAxMjUzODk4Nn0.jII7SbPyktiAuUrKirqQ6eD7_2uu-Mb8crpnT0MkMfo"
 supabase: Client = create_client(url, key)
 
 
 
-@app.route('/movies', methods=['POST'])
+"""@app.route('/movies', methods=['POST'])
 def get_movies():
     try:
         request_data = request.json
@@ -30,8 +31,27 @@ def get_movies():
 
         return jsonify(diccionario), 200
     except Exception as e:
-        return jsonify({"error": str(e)}), 404
-    
+        return jsonify({"error": str(e)}), 404"""
+
+
+@app.route('/movies', methods=['POST'])
+def consulta_IA():
+    try:
+        request_data = request.json
+        movie_generos = request_data['generos']
+        recommendations = IA(movie_generos)
+        # Obtener los nombres de las películas recomendadas
+        recommended_movie_titles = recommendations["movies"]
+
+        diccionario= {"movies":[]}
+        for title in recommended_movie_titles:
+            data_to_insert = {"title": title}
+            diccionario["movies"].append(title)
+            data, count = supabase.table("movies").insert(data_to_insert).execute()
+
+        return jsonify(diccionario), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 404    
 
 def create_app():
     return app
