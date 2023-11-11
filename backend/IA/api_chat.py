927c80b69669f9cd830b44e0970c7d274944ea56
JLeandroJM <jose.machaca.s@utec.edu.pe>
2023-11-10 20:58:23 -0500

    commit con otra peli y lugares

--- backend/IA/api_chat.py
+++ backend/IA/api_chat.py
@@ -1,8 +1,18 @@
+import requests
+from dotenv import load_dotenv
+import os
+import re
 import openai
 import time
-import os
-from dotenv import load_dotenv
+
 load_dotenv()
+
+# Set up the OpenAI API key
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# OMDB API key
+omdb_api_key = os.getenv("OMDB_API_KEY")
+
 
 clave = "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"
 # Configura la API key
@@ -57,10 +67,173 @@ def IA_generos():
     promt="Recomiendame solo los nombres de 5 generos "
 
     pass
+
+
+
+def get_movie_director(title):
+
+    omdb_url = f"http://www.omdbapi.com/?t={title}&apikey={omdb_api_key}"
+    response = requests.get(omdb_url)
+    
+    if response.status_code == 200:
+        movie_data = response.json()
+        director = movie_data.get("Director", "")
+        return director
+    else:
+        return None
+
+
+def IA_director_movies(user, directors, top_n=10):
+    # Function to generate a top N list of movie recommendations based on a list of directors for a user
+    prompt = f"Recomiéndame las mejores {top_n} películas dirigidas por {' y '.join(directors)} para {user}."
+
+    start_time = time.time()
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": prompt},
+        ],
+        temperature=0.7
+    )
+
+    end_time = time.time()
+
+    respuesta_generada = response['choices'][0]['message']['content']
+
+    movie_titles = [title.strip('" ') for title in re.findall(r'"([^"]*)"', respuesta_generada)[:top_n]]
+
+    respuesta = {
+        "movies": movie_titles,
+        "time": end_time - start_time
+    }
+
+    return respuesta
+
+
+
+def get_movies_by_awards(awards, top_n=10):
+
+    awards_str = ",".join(awards)
+    omdb_url = f"http://www.omdbapi.com/?s=&apikey={omdb_api_key}&awards={awards_str}&type=movie"
+    response = requests.get(omdb_url)
+    
+    if response.status_code == 200:
+        movies_data = response.json().get("Search", [])
+
+        movie_titles = [movie.get("Title", "").strip('" ') for movie in movies_data]
+        return movie_titles[:top_n] 
+    else:
+        return []  
+
+def IA_awards_movies(user, awards, top_n=10):
+
+    movie_titles = get_movies_by_awards(awards, top_n)
+
+    awards_str = ", ".join(awards)
+    prompt = f"Recomiéndame las mejores {top_n} películas que han ganado premios {awards_str} para {user}."
+
+    start_time = time.time()
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": prompt},
+        ],
+        temperature=0.7
+    )
+
+    end_time = time.time()
+
+    respuesta_generada = response['choices'][0]['message']['content']
     
 
+    additional_movie_titles = [title.strip('" ') for title in re.findall(r'"([^"]*)"', respuesta_generada)[:top_n - len(movie_titles)]]
+
+    respuesta = {
+        "movies": movie_titles + additional_movie_titles,
+        "time": end_time - start_time
+    }
+
+    return respuesta
+
+def IA_lugar_grabacion(user, lugares, top_n=10):
+    # Genera una respuesta a partir de un prompt
+
+    promt="Recomiendame solo los nombres de 10 peliculas sin informacion antes ni despues , que hayan sido grabadas en "
+    
+    
+    for i in lugares:
+        promt = promt + i + ","     
+    
+    promt+="y devuelvemelo en formato python de lista, en una sola linea, sin informacion extra, sin corchetes, separado por comas, sin comillas."
+
+    print(promt)
+
+    start_time = time.time()
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": promt },
+        ],
+        temperature=0.7
+    )
+
+    end_time = time.time()
+
+    print("Tiempo de ejecucion: ", end_time - start_time , " segundos")
+
+    # Extraer la respuesta generada por el modelo, la cual se encuentra en el primer elemento de la lista
+    respuesta_generada = response['choices'][0]['message']['content']
+
+    # Mostrar la respuesta
+    respuesta_generada = respuesta_generada.split(",")
+
+    respuesta={
+        "movies":respuesta_generada,
+        "time": end_time - start_time
+    }
+
+    return respuesta
 
 
+def IA_same_peliculas(peliculas, top_n=10):
+    # Genera una respuesta a partir de un prompt
 
+    promt="Recomiendame solo los nombres de 10 peliculas sin informacion antes ni despues , que sean similares a estas otras peliculas "
+    
+    
+    for i in peliculas:
+        promt = promt + i + ","     
+    
+    promt+="y devuelvemelo en formato python de lista, en una sola linea, sin informacion extra, sin corchetes, separado por comas, sin comillas."
 
+    print(promt)
 
+    start_time = time.time()
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": promt },
+        ],
+        temperature=0.7
+    )
+
+    end_time = time.time()
+
+    print("Tiempo de ejecucion: ", end_time - start_time , " segundos")
+
+    # Extraer la respuesta generada por el modelo, la cual se encuentra en el primer elemento de la lista
+    respuesta_generada = response['choices'][0]['message']['content']
+
+    # Mostrar la respuesta
+    respuesta_generada = respuesta_generada.split(",")
+
+    respuesta={
+        "movies":respuesta_generada,
+        "time": end_time - start_time
+    }
+
+    return respuesta
--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@
 from flask import Flask, jsonify, request
 from supabase import create_client, Client
 from DB.peliculas_usuarios import usuario_peliculas
-from IA.api_chat import IA_peliculas,IA_generos
+from IA.api_chat import IA_peliculas,IA_generos, IA_director_movies, IA_awards_movies,IA_lugar_grabacion,IA_same_peliculas
 import os 
 from dotenv import load_dotenv
 load_dotenv()
@@ -37,6 +37,62 @@ def obtener_generos():
         generos=IA_generos()
         print(generos)
         return jsonify(generos), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 404
+
+
+@app.route('/director_movies', methods=['POST'])
+def obtener_peliculas_por_director():
+    try:
+        request_data = request.json
+        usuario = request_data['usuario']
+        directores = request_data['directores']
+        director_movies = IA_director_movies(usuario, directores, top_n=10)
+        print(f"Top 10 películas dirigidas por {', '.join(directores)} para {usuario}", director_movies)
+        return jsonify(director_movies), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 404
+
+
+
+@app.route('/awards_movies', methods=['POST'])
+def obtener_peliculas_por_premios():
+    try:
+        request_data = request.json
+        usuario = request_data['usuario']
+        awards = request_data['awards']
+        awards_movies = IA_awards_movies(usuario, awards)
+        print(f"Top 10 películas con premios {', '.join(awards)} para {usuario}", awards_movies)
+        return jsonify(awards_movies), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 404
+
+
+@app.route('/lugar_grabacion', methods=['POST'])
+def obtener_peliculas_por_lugar_grabacion():
+    try:
+        request_data = request.json
+        usuario = request_data['usuario']
+        lugares = request_data['lugares']
+        lugar_grabacion_movies = IA_lugar_grabacion(usuario, lugares, top_n=10)
+        print(f"Top 10 películas grabadas en {', '.join(lugares)} para {usuario}", lugar_grabacion_movies)
+        return jsonify(lugar_grabacion_movies), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 404
+
+
+@app.route('/same_movies', methods=['POST'])
+def obtener_recomendaciones_por_peliculas():
+    try:
+        request_data = request.json
+        usuario = request_data['usuario']
+        peliculas = request_data['movies']
+        
+        # Llamada a la función IA_peliculas
+        recomendaciones = IA_same_peliculas(peliculas, top_n=10)
+
+        print(f"Top 10 recomendaciones basadas en películas para {usuario}", recomendaciones)
+        return jsonify(recomendaciones), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 404
 
